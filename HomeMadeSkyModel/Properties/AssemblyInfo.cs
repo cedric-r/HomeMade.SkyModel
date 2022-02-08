7c85a98da91a111ad9ff6c9d02d43d413910df2e
Cedric Raguenaud <32923629+cedric-r@users.noreply.github.com>
2022-02-08 13:16:09 +0100

    Added installer.

--- HomeMadeSkyModel/Properties/AssemblyInfo.cs
+++ HomeMadeSkyModel/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HomeMadeSkyModel")]
+[assembly: AssemblyTitle("HomeMade.SkyModel")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HomeMadeSkyModel")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyProduct("HomeMade.SkyModel")]
+[assembly: AssemblyCopyright("Copyright © Cedric Raguenaud 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
